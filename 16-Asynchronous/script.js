ad98c7d1bff22e307ff219ed51b2f4cb1559cbc0
Murali Raju <nlmrkraju@gmail.com>
2021-11-01 22:14:37 -0400

    completed handling rejected promises

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -6,38 +6,87 @@ const countriesContainer = document.querySelector('.countries');
 ///////////////////////////////////////
 //? XML http request funciton old shool way.
 
-const getCountryData = function (country) {
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
-  request.send();
+// const getCountryData = function (country) {
+//   const request = new XMLHttpRequest();
+//   request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+//   request.send();
 
-  request.addEventListener('load', function () {
-    const [data] = JSON.parse(this.responseText);
-    const { languages } = Object.values(data.languages).reduce(
-      (map, item) => {
-        map.languages.push(item);
+//   request.addEventListener('load', function () {
+//     const [data] = JSON.parse(this.responseText);
+//     const { languages } = Object.values(data.languages).reduce(
+//       (map, item) => {
+//         map.languages.push(item);
 
-        return map;
-      },
-      { languages: [] }
-    );
+//         return map;
+//       },
+//       { languages: [] }
+//     );
 
-    const { currencyName, currencySymbol } = Object.values(
-      data.currencies
-    ).reduce(
-      (map, item) => {
-        map.currencyName.push(item.name);
-        map.currencySymbol.push(item.symbol);
+//     const { currencyName, currencySymbol } = Object.values(
+//       data.currencies
+//     ).reduce(
+//       (map, item) => {
+//         map.currencyName.push(item.name);
+//         map.currencySymbol.push(item.symbol);
 
-        return map;
-      },
-      { currencyName: [], currencySymbol: [] }
-    );
+//         return map;
+//       },
+//       { currencyName: [], currencySymbol: [] }
+//     );
 
-    console.log(data);
+//     console.log(data);
 
-    const html = `
-  <article class="country">
+//     const html = `
+//   <article class="country">
+//           <img class="country__img" src="${data.flags.png}" />
+//           <div class="country__data">
+//             <h3 class="country__name">${data.name.common}</h3>
+//             <h4 class="country__region">${data.region}</h4>
+//             <p class="country__row"><span>👫</span>${(
+//               +data.population / 1000000
+//             ).toFixed(1)} people</p>
+//             <p class="country__row"><span>🗣️</span>${languages[0]}</p>
+//             <p class="country__row"><span>💰</span>${currencyName[0]}</p>
+//           </div>
+//         </article>
+//   `;
+
+//     countriesContainer.insertAdjacentHTML('beforeend', html);
+//     countriesContainer.style.opacity = 1;
+//   });
+// };
+
+// getCountryData('portugal');
+// getCountryData('usa');
+// getCountryData('india');
+
+/*
+const renderCountry = function (data, className = '') {
+  // const { languages } = Object.values(data.languages).reduce(
+  //   (map, item) => {
+  //     map.languages.push(item);
+
+  //     return map;
+  //   },
+  //   { languages: [] }
+  // );
+
+  // const { currencyName, currencySymbol } = Object.values(
+  //   data.currencies
+  // ).reduce(
+  //   (map, item) => {
+  //     map.currencyName.push(item.name);
+  //     map.currencySymbol.push(item.symbol);
+
+  //     return map;
+  //   },
+  //   { currencyName: [], currencySymbol: [] }
+  // );
+
+  //console.log(Object.values(data.currencies)[0].name);
+
+  const html = `
+  <article class="country ${className}">
           <img class="country__img" src="${data.flags.png}" />
           <div class="country__data">
             <h3 class="country__name">${data.name.common}</h3>
@@ -45,17 +94,141 @@ const getCountryData = function (country) {
             <p class="country__row"><span>👫</span>${(
               +data.population / 1000000
             ).toFixed(1)} people</p>
-            <p class="country__row"><span>🗣️</span>${languages[0]}</p>
-            <p class="country__row"><span>💰</span>${currencyName[0]}</p>
+            <p class="country__row"><span>🗣️</span>${
+              Object.values(data.languages)[0]
+            }</p>
+            <p class="country__row"><span>💰</span>${
+              Object.values(data.currencies)[0].name
+            }</p>
           </div>
         </article>
   `;
 
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbour = function (country) {
+  //? AJAX cal country 1
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    //? Render country 1
+    renderCountry(data);
+
+    //? Get ndighbour country (2)
+    const [neighbour] = data.borders;
+    if (!neighbour) return;
+
+    //? AJAX cal country 2
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      const [data2] = JSON.parse(this.responseText);
+      console.log(data2);
+
+      //? Render country 2
+      renderCountry(data2, 'neighbour');
+    });
   });
 };
 
-getCountryData('portugal');
-getCountryData('usa');
-getCountryData('india');
+//getCountryAndNeighbour('portugal');
+getCountryAndNeighbour('usa');
+*/
+
+// const getCountryData = function (country) {
+//   const request = new XMLHttpRequest();
+//   request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+//   request.send();
+
+/*
+const request = fetch('https://restcountries.com/v3.1/name/portugal');
+console.log(request);
+*/
+
+//? Promise: An object that is used as as placeholder for the future result of an asynchronous operation.
+//? Promise: A container for an asynchronously delivered value.
+//? Promise: A container for a future value.
+
+//! Consuming promises
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+};
+
+const renderCountry = function (data, className = '') {
+  const html = `
+  <article class="country ${className}">
+          <img class="country__img" src="${data.flags.png}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name.common}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(
+              +data.population / 1000000
+            ).toFixed(1)} people</p>
+            <p class="country__row"><span>🗣️</span>${
+              Object.values(data.languages)[0]
+            }</p>
+            <p class="country__row"><span>💰</span>${
+              Object.values(data.currencies)[0].name
+            }</p>
+          </div>
+        </article>
+  `;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+};
+
+// const getCountryData = function (country) {
+//   fetch(`https://restcountries.com/v3.1/name/${country}`)
+//     .then(function (response) {
+//       console.log(response);
+//       // console.log(response.json());
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//       renderCountry(data[0]);
+//     });
+// };
+
+const getCountryData = function (country) {
+  //? Country 1
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(response => response.json())
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders[0];
+      if (!neighbour) return;
+
+      //? Country 2
+      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => {
+      renderCountry(data[0], 'neighbour');
+    })
+    .catch(err => {
+      console.log(`${err}🌋🌋🌋`);
+      renderError(`Something went wrong 🌋🌋🌋 ${err.message}. Try again!`);
+    })
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
+    });
+};
+
+btn.addEventListener('click', function () {
+  getCountryData('portugal');
+});
+
+getCountryData('addsssee');
+
+//! Chaining promises
+//! Handling rejected promises
