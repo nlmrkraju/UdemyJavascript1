620da0701e0c7738b20a7cd5359736495cc85bc7
Murali Raju <nlmrkraju@gmail.com>
2021-11-15 19:21:44 -0500

    Configiring Babel and Polyfilling

--- 17-Modern-JS-Modules-Tooling/script.js
+++ 17-Modern-JS-Modules-Tooling/script.js
@@ -26,14 +26,14 @@ console.log(shoppingCart.totalPrice);
 
 //import add, { addToCart, totalPrice as price, tq } from './shoppingCart.js';
 //console.log(price);
-/*
+
 import add, { cart } from './shoppingCart.js';
 add('pizza', 2);
 add('bread', 5);
 add('apples', 4);
 
 console.log(cart);
-*/
+
 //! Top-Level await
 /*
 //? Atleast in moudles from ES2022 await can be used outside async
@@ -134,3 +134,24 @@ console.log(stateDeepClone);
 if (module.hot) {
   module.hot.accept();
 }
+
+class Person {
+  greeting = 'Hey';
+  constructor(name) {
+    this.name = name;
+    console.log(`${this.greeting}, ${this.name}`);
+  }
+}
+
+const jonas = new Person('Jonas');
+
+console.log('Jonas' ?? null);
+
+console.log(cart.find(el => el.quantity >= 2));
+Promise.resolve('TEST').then(x => console.log(x));
+import 'core-js/stable';
+// import 'core-js/stable/array/find';
+// import 'core-js/stable/promise';
+
+//? Plifilling async functions
+import 'regenerator-runtime/runtime';
