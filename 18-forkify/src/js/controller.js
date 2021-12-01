d46d5526d5a5b79d771c58604034e45907e244ed
Murali Raju <nlmrkraju@gmail.com>
2021-11-30 21:29:36 -0500

    Completed search results

--- 18-forkify/src/js/controller.js
+++ 18-forkify/src/js/controller.js
@@ -1,6 +1,7 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
 
 import 'core-js/stable'; //? Polyfilling for everything else
 import 'regenerator-runtime/runtime'; //? Polyfilling for async/await
@@ -16,6 +17,12 @@ import { async } from 'regenerator-runtime';
 //! Event Handlers in MVC: Publisher-Subscriber Pattern
 //! Implementing Error and Success Messages
 //! Implementing Search Results - Part 1
+//! Implementing Search Results - Part 2
+
+if (module.hot) {
+  module.hot.accept();
+}
+
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
@@ -35,6 +42,8 @@ const controlRecipes = async function () {
 
 const controlSearchResults = async function () {
   try {
+    resultsView.renderSpinner();
+
     //? 1) Get search query
     const query = searchView.getQuery();
     if (!query) return;
@@ -43,7 +52,7 @@ const controlSearchResults = async function () {
     await model.loadSearchResults(query);
 
     //? 3) Render results
-    console.log(model.state.search.results);
+    resultsView.render(model.state.search.results);
   } catch (err) {
     console.log(err);
   }
