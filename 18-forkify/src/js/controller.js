664c8034efd51be9e85a8e4bacd7892cee3c898b
Murali Raju <nlmrkraju@gmail.com>
2021-12-05 15:16:02 -0500

    Bookmarks part2

--- 18-forkify/src/js/controller.js
+++ 18-forkify/src/js/controller.js
@@ -3,6 +3,7 @@ import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
 import paginationView from './views/paginationView.js';
+import bookmarksView from './views/bookmarksView.js';
 
 import 'core-js/stable'; //? Polyfilling for everything else
 import 'regenerator-runtime/runtime'; //? Polyfilling for async/await
@@ -36,6 +37,7 @@ const controlRecipes = async function () {
 
     //? 0) Update results view to mark selected search result
     resultsView.update(model.getSearchResultsPage());
+    bookmarksView.update(model.state.bookmarks);
 
     //? 1) Loading Recipe
     await model.loadRecipe(id);
@@ -87,10 +89,15 @@ const controlServings = function (newServings) {
 };
 
 const controlAddBookmark = function () {
+  //? 1) Add/remove bookmark
   if (!model.state.recipe.bookmarked) model.addBookmark(model.state.recipe);
   else model.deleteBookmark(model.state.recipe.id);
-  console.log(model.state.recipe);
+
+  //? 2) Updte recipe view
   recipeView.update(model.state.recipe);
+
+  //? 3) Render bookmarks
+  bookmarksView.render(model.state.bookmarks);
 };
 
 const init = function () {
